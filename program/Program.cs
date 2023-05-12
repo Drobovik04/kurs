7acebff9eb21e4e9cae145a546503c22f857349e
Drobovik04 <misle92@mail.ru>
2023-05-12 19:50:01 +0300

    add all

---
+++ program/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using static program.Utilities;
+using System.Diagnostics;
+namespace program
+{
+    class Program
+    {
+        static List<List<int>> ReadFromFile(StreamReader stream)
+        {
+            List<List<int>> list = new List<List<int>>();
+            while (!stream.EndOfStream)
+                list.Add(stream.ReadLine().Split(' ').Select(int.Parse).ToList()); // в цикле парсим каждую строку входного файла, которую затем преобразуем в один элемент массива, повторяем до конца файла
+            return list; // возвращаем полученный массив
+        }
+        static bool SequentialSearch(List<int> array, int element, out int index, out int comparisons)
+        {
+            comparisons = 0;
+            for (int i = 0; i < array.Count; i++)
+            {
+                comparisons++; // считаем количество сравнений
+                if (array[i] == element) // проходим по всем элементам массива и сравниваем их с искомым, если находим, то запоминаем индекс и возвращаем положительный ответ
+                {
+                    index = i;
+                    return true;
+                }
+            }
+            index = -1;
+            return false; // если мы не смогли найти искомый элемент, возвращаем false и индекс -1
+        }
+        static bool BinarySearch(List<int> array, int element, out int index, out int comparisons)
+        {
+            int l = 0; // левый указатель
+            int r = array.Count - 1; // правый указатель
+            comparisons = 0;
+            while (l <= r) // пока между указателями расстояние не отрицательно
+            {
+                int c = l + (r - l) / 2; // находим центральный элемент
+                comparisons++;
+                if (array[c] == element) // сравниваем центральный элемент с искомымы, если нашли, то возвращаем ответ true и индекс элемента
+                {
+                    index = c;
+                    return true;
+                }
+                if (element < array[c]) // если мы не нашли элемент, то сдвигаем границу поиска, ориентируясь на отношение между центральным и искомым элементом (в данном случае элемент меньше центрального, то сдвигаем правую границу, в ином случае левую границу)
+                {
+                    r = c - 1;
+                }
+                else
+                {
+                    l = c + 1; //сдвигаем леву границу
+                }
+            }
+            index = -1;
+            return false; // если так и не нашли элемент - возвращаем false и индекс -1
+        }
+        static void Main(string[] args)
+        {
+            int index;
+            int comparisons;
+            //Utilities.Generate();
+            List<List<int>> l = ReadFromFile(new StreamReader("in.txt")); // читаем данные с входного файла
+            if (l.Count == 0) // проверка на пустоту файла
+            { 
+                Console.WriteLine("нет массива"); 
+                Console.ReadKey(); 
+                return;
+            }
+            Console.WriteLine("Введите элемент для поиска");
+            int element = Convert.ToInt32(Console.ReadLine()); // ввдоим элемент для поиска
+            SequentialSearch(new List<int>(1), 1, out index, out comparisons); // вызываем функции для предворительной компиляции, для более точных значений позднее
+            BinarySearch(new List<int>(1), 1, out index, out comparisons);
+            int[,] m = new int[l.Count,4]; // массив, в котором будем хранить результаты тестов
+            Stopwatch sw = new Stopwatch(); // Счетчик прошедшего времени
+            float[,] time = new float[l.Count,2];
+            for (int i = 0; i < l.Count; i++)
+            {
+                List<int> arr = l[i]; // построчно считываем последовательности из массива
+                sw.Start(); // начинаем замер времени
+                SequentialSearch(arr, element, out index, out comparisons); // вызов алгоритма поиска
+                sw.Stop(); // останавливаем счетчик
+                m[i, 0] = comparisons; // записываем кол-во сравнений
+                m[i, 2] = index; // записываем индекс
+                m[i, 3] = arr.Count; // записываем длину массива
+                time[i, 0] = (float)sw.Elapsed.Ticks/TimeSpan.TicksPerMillisecond; // записываем время
+                sw.Reset(); // сбрасываем счетчик
+                sw.Start(); // начинаем замер времени
+                BinarySearch(arr, element, out index, out comparisons); // вызов алгоритма поиска
+                sw.Stop(); // останавливаем счетчик
+                time[i, 1] = (float)sw.Elapsed.Ticks / TimeSpan.TicksPerMillisecond; // записываем время
+                sw.Reset(); // сбрасываем счетчик
+                m[i, 1] = comparisons; // записываем кол-во сравнений
+            }
+            PrintLine(); // отрисовываем верхнюю границу таблицы
+            PrintRow(true,"Алгоритм","Длина массива","Кол-во итераций","Найден ли элемент", "Время (мс)"); // рисуем названия столбцов
+            for (int i = 0; i < m.GetLength(0); i++) // перебираем полученные данные
+            {
+                PrintRow(false,"Линейный", m[i, 3].ToString(), m[i, 0].ToString(), m[i, 2] != -1 ? "Yes" : "No", time[i, 0].ToString("0.00000")); // выводим данные для алгоритма линейного поиска при обработке i-го массива 
+                PrintRow(false,"Бинарный", m[i, 3].ToString(), m[i, 1].ToString(), m[i, 2] != -1 ? "Yes" : "No", time[i, 1].ToString("0.00000")); // выводим данные для алгоритма бинарного поиска при обработке i-го массива 
+            }
+            PrintLine(); // отрисовываем нижнюю границу таблицы
+            Console.ReadKey();
+        }
+    }
+}
---
+++ program/Utilities.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program
+{
+    class Utilities
+    {
+        static int tableWidth = 90;
+        static StreamWriter writer = new StreamWriter(new FileStream("out.txt", FileMode.Create));
+        public static void Generate()
+        {
+            File.WriteAllText("in.txt", string.Empty);
+            StreamWriter strw = new StreamWriter("in.txt");
+            Random r = new Random();
+            int[] l = new int[10];
+            for (int i = 0; i < 10; i++)
+            {
+                l[i] = r.Next(10000);
+            }
+            Array.Sort(l);
+            strw.WriteLine(string.Join(" ", l.Select(x => x.ToString())));
+            l = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                l[i] = r.Next(10000);
+            }
+            Array.Sort(l);
+            strw.WriteLine(string.Join(" ", l.Select(x => x.ToString())));
+            l = new int[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                l[i] = r.Next(10000);
+            }
+            Array.Sort(l);
+            strw.WriteLine(string.Join(" ", l.Select(x => x.ToString())));
+            l = new int[10000];
+            for (int i = 0; i < 10000; i++)
+            {
+                l[i] = r.Next(10000);
+            }
+            Array.Sort(l);
+            strw.WriteLine(string.Join(" ", l.Select(x => x.ToString())));
+            l = new int[100000];
+            for (int i = 0; i < 100000; i++)
+            {
+                l[i] = r.Next(100000);
+            }
+            Array.Sort(l);
+            strw.WriteLine(string.Join(" ", l.Select(x => x.ToString())));
+            strw.Flush();
+            strw.Close();
+        }
+        public static void PrintLine()
+        {
+            Console.WriteLine(new string('-', tableWidth));
+            writer.WriteLine(new string('-', tableWidth));
+            writer.Flush();
+        }
+
+        public static void PrintRow(bool align, params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "|";
+            if (align)
+            {
+                foreach (string column in columns)
+                {
+                    row += AlignCentre(column, width) + "|";
+                }
+            }
+            else
+            {
+                foreach (string column in columns)
+                {
+                    row += AlignLeft(column, width) + "|";
+                }
+            }
+
+            Console.WriteLine(row);
+            writer.WriteLine(row);
+            writer.Flush();
+        }
+
+        static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+        static string AlignLeft(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text+new string(' ',width-text.Length);
+            }
+        }
+    }
+}
